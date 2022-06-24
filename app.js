549faa7574d3e5b3f4598ba43e484c399465f122
unknown <leviettrung.social@gmail.com>
2022-06-24 19:58:12 +0700

    \\\

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ app.get('*', (req, res) => {
 
 
 io.on('connection', (socket) => {
-    // console.log('a user connected');
-    // console.log(socket.id);
+     console.log('-------------------------a user connected------------------------------------');
+     console.log(socket.id);
     //    get header cookie
     let cookie = socket.handshake.headers.cookie;
     if (cookie) {
@@ -66,16 +66,16 @@ io.on('connection', (socket) => {
             useronl[objData.email] = [];
         }
         useronl[objData.email].push(socket.id);
-        // console.log(useronl);
+         console.log(useronl);
         socket.on('disconnect', () => {
-            // console.log('a user disconnected');
-            // console.log(socket.id);
+             console.log('-------------------------a user disconnected------------------------------------');
+             console.log(socket.id);
             useronl[objData.email].splice(useronl[objData.email].indexOf(socket.id), 1);
             //if array empty, delete user r
             if (useronl[objData.email].length == 0) {
                 delete useronl[objData.email];
             }
-            // console.log(useronl);
+             console.log(useronl);
             io.emit('count', Object.keys(useronl).length);
         });
         io.emit('count', Object.keys(useronl).length);
